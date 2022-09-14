17401e65ff55da72ae14bc0a44e32b82c03b3bc2
Stef Castillo <estefan.castillo@yahoo.com>
2022-09-13 21:42:19 -0500

    creed 1st commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// Middleware
+app.use(express.static(path.join(__dirname, "../public")))
+
+
+const port = process.env.PORT || 1994;
+
+app.listen(port, () => {
+    console.log(`We are on port ${port}`);
+})
